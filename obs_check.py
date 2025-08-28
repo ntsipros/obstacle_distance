a2cd86819a1b7cadf74637a3fab92e2745635f8f
ntsipros <102686341+ntsipros@users.noreply.github.com>
2025-08-28 16:47:36 +0300

    Update obs_check.py

--- obs_check.py
+++ obs_check.py
@@ -20,7 +20,7 @@ def obstacle_check(icao, rwy, obs_lat, obs_lon, height, df):
         return lat1, lon1
     
     def get_airport_info(df, icao, rwy):
-        thr_lon = df.loc[(df['Icao'] == icao) & (df['Name4'] == rwy) & (df['Ident'].isnull())]['ThresholdLongitude'].values[0]
+      thr_lon = df.loc[(df['Icao'] == icao) & (df['Name4'] == rwy) & (df['Ident'].isnull())]['ThresholdLongitude'].values[0]
       thr_lat = df.loc[(df['Icao'] == icao) & (df['Name4'] == rwy) & (df['Ident'].isnull())]['ThresholdLatitude'].values[0]
       magnetic_variation = df.loc[(df['Icao'] == icao)]['MagneticVariation'].values[0]
       direction = magnetic_variation[-1]
